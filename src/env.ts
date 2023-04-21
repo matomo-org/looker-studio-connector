6531f57414381e156cf3a8e379176d63cf1aa1db
diosmosis <diosmosis@users.noreply.github.com>
2023-04-21 03:45:45 -0700

    default CONFIG_REQUEST_CACHE_TTL_SECS to 60s

--- src/env.ts
+++ src/env.ts
@@ -9,7 +9,7 @@ const properties = PropertiesService.getScriptProperties();
 
 export default <Record<string, string>>{
   DEBUG: properties.getProperty('DEBUG') || process.env.DEBUG || '0',
-  CONFIG_REQUEST_CACHE_TTL_SECS: properties.getProperty('CONFIG_REQUEST_CACHE_TTL_SECS') || process.env.CONFIG_REQUEST_CACHE_TTL_SECS || '300',
+  CONFIG_REQUEST_CACHE_TTL_SECS: properties.getProperty('CONFIG_REQUEST_CACHE_TTL_SECS') || process.env.CONFIG_REQUEST_CACHE_TTL_SECS || '60',
   MAX_ROWS_TO_FETCH_PER_REQUEST: properties.getProperty('MAX_ROWS_TO_FETCH_PER_REQUEST') || process.env.MAX_ROWS_TO_FETCH_PER_REQUEST || '100000',
   SCRIPT_RUNTIME_LIMIT: properties.getProperty('SCRIPT_RUNTIME_LIMIT') || process.env.SCRIPT_RUNTIME_LIMIT || '350',
   API_REQUEST_SOURCE_IDENTIFIER: properties.getProperty('API_REQUEST_SOURCE_IDENTIFIER') || process.env.API_REQUEST_SOURCE_IDENTIFIER || 'fromLooker',
