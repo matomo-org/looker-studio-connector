e55b058b494a78480292ab693052e7ff0c996cdc
diosmosis <diosmosis@users.noreply.github.com>
2023-05-03 08:10:11 -0700

    a little more console output

--- scripts/update_ci_secrets.ts
+++ scripts/update_ci_secrets.ts
@@ -63,10 +63,14 @@ async function main() {
   let secretChangeFailed = false;
   if (hasSecretChanged('CLASP_CREDENTIALS', `${process.env.HOME}/.clasprc.json`)) {
     secretChangeFailed |= await !saveSecret('CLASP_CREDENTIALS', `${process.env.HOME}/.clasprc.json`);
+  } else {
+    console.log('CLASP_CREDENTIALS has not changed');
   }
 
   if (hasSecretChanged('CLASP_CREDENTIALS_LOCAL', '.clasprc.json')) {
     secretChangeFailed |= await !saveSecret('CLASP_CREDENTIALS_LOCAL', '.clasprc.json');
+  } else {
+    console.log('CLASP_CREDENTIALS_LOCAL has not changed');
   }
 
   return secretChangeFailed ? 1 : 0;
