06530bdfe8af147d0d9d7c8707b332553bf0aab7
diosmosis <diosmosis@users.noreply.github.com>
2023-05-03 08:36:53 -0700

    try using orgs base?

--- scripts/update_ci_secrets.ts
+++ scripts/update_ci_secrets.ts
@@ -21,7 +21,7 @@ import sodium from 'libsodium-wrappers';
 async function main() {
   await sodium.ready;
 
-  const base = 'repos';
+  const base = 'orgs';
   const repo = process.env.GITHUB_REPOSITORY;
 
   const octokit = new Octokit({ auth: process.env.REPO_TOKEN });
