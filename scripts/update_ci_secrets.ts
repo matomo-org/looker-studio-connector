99375e28f05e66ebcd850240651467188165cca1
diosmosis <diosmosis@users.noreply.github.com>
2023-05-03 07:52:38 -0700

    fill out update_ci_secrets.ts script and use in github actions

--- scripts/update_ci_secrets.ts
+++ scripts/update_ci_secrets.ts
@@ -7,4 +7,72 @@
 
 // this script is used to update the repository secrets that allow running clasp
 // in github actions. it is run within github actions.
+//
+// we cannot login to clasp the normal way w/o a web browser, which makes logging in
+// within github actions harder. instead, we login locally, then set the oauth
+// credentials file as a secret. if the credentials become stale, they will be refreshed
+// by clasp. we detect when this happens, and update the secret, so the build can
+// continue running w/o a human doing something.
 
+import * as fs from 'fs';
+import { Octokit } from 'octokit';
+import sodium from 'tweetsodium';
+
+async function main() {
+  const base = 'repos';
+  const repo = process.env.GITHUB_REPOSITORY;
+
+  const octokit = new Octokit({ auth: process.env.REPO_TOKEN });
+
+  let publicKeyResponse = await octokit.request('GET /:base/:repo/actions/secrets/public-key', { base, repo });
+  const { key_id, key } = publicKeyResponse.data;
+
+  const hasSecretChanged = (secretEnvVarName: string, secretFile: string) => {
+    const secretInEnv = process.env[secretEnvVarName];
+    const secretInFile = fs.readFileSync(secretFile).toString('utf-8');
+    return secretInFile.trim().length && secretInEnv !== secretInFile;
+  };
+
+  const saveSecret = async (secretEnvVarName: string, secretFile: string) => {
+    console.log(`${secretEnvVarName} secret changed in file, updating repo secret...`);
+
+    const newSecretValue = fs.readFileSync(secretFile);
+
+    const keyBytes = Buffer.from(key, 'base64');
+    const encryptedBytes = Buffer.from(sodium.seal(newSecretValue, keyBytes)).toString('base64');
+
+    const response = await octokit.request('PUT /:base/:repo/actions/secrets/:name', {
+      base: base,
+      repo: repo,
+      name: secretEnvVarName,
+      data: {
+        encrypted_value: encryptedBytes,
+        key_id,
+      },
+    });
+
+    if (response.status >= 400) {
+      console.log('failed to save secret:', response.data);
+      return false;
+    } else {
+      console.log('secret saved successfully');
+      return true;
+    }
+  }
+
+  let secretChangeFailed = false;
+  if (hasSecretChanged('CLASP_CREDENTIALS', `${process.env.HOME}/.clasprc.json`)) {
+    secretChangeFailed |= await !saveSecret('CLASP_CREDENTIALS', `${process.env.HOME}/.clasprc.json`);
+  }
+
+  if (hasSecretChanged('CLASP_CREDENTIALS_LOCAL', '.clasprc.json')) {
+    secretChangeFailed |= await !saveSecret('CLASP_CREDENTIALS_LOCAL', '.clasprc.json');
+  }
+
+  return secretChangeFailed ? 1 : 0;
+}
+
+main()
+  .then((code) => {
+    process.exit(code);
+  });
