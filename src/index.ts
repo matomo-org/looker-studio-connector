a34cf013292802fde658080d690c9aa000910a9f
diosmosis <diosmosis@users.noreply.github.com>
2024-01-22 08:11:47 -0800

    add unit tests for change

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@
  * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
  */
 
+export { extractBasicAuthFromUrl } from './api';
 export * from './auth';
 export * from './config';
 export * from './data';
