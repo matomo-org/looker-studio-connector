a622d0f487a95916576eabf3fc8022e96edd425b
diosmosis <diosmosis@users.noreply.github.com>
2024-01-21 17:58:36 -0800

    allow process to be undefined (if no .env file is used)

--- tests/env.ts
+++ tests/env.ts
@@ -5,8 +5,10 @@
  * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
  */
 
+const env = typeof process === 'undefined' ? null : process.env;
+
 export default {
-  APPSCRIPT_TEST_MATOMO: process.env.APPSCRIPT_TEST_MATOMO || 'https://demo.matomo.cloud/',
-  APPSCRIPT_TEST_TOKEN: process.env.APPSCRIPT_TEST_TOKEN || 'anonymous',
-  APPSCRIPT_TEST_IDSITE: process.env.APPSCRIPT_TEST_IDSITE || 1,
+  APPSCRIPT_TEST_MATOMO: env?.APPSCRIPT_TEST_MATOMO || 'https://demo.matomo.cloud/',
+  APPSCRIPT_TEST_TOKEN: env?.APPSCRIPT_TEST_TOKEN || 'anonymous',
+  APPSCRIPT_TEST_IDSITE: env?.APPSCRIPT_TEST_IDSITE || 1,
 };
