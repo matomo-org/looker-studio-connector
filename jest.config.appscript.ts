72eebb6e18bc5ea5f64551a57e6f443b041d7156
diosmosis <diosmosis@users.noreply.github.com>
2024-09-11 18:27:15 -0700

    increase global test timeout

--- jest.config.appscript.ts
+++ jest.config.appscript.ts
@@ -9,7 +9,7 @@ export default {
   rootDir: 'tests/appscript',
   preset: 'ts-jest/presets/js-with-ts-esm',
   testEnvironment: 'node',
-  testTimeout: 120000,
+  testTimeout: 180000,
   globalSetup: '../globalSetup.js',
   globalTeardown: '../globalTeardown.js',
   maxWorkers: 1, // since we run our tests within apps script, we can't run them in parallel
